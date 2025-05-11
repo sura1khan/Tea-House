af5c23d61fe4f7ff3f95a78e5325bbaf40c65c98
Suraia <khansuraia645@gmail.com>
2025-05-11 21:03:12 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,14 @@
+const sections = document.querySelectorAll('section')
+for(const section of sections){
+    section.style.backgroundColor='white';
+    section.style.color='black';
+}
+const button= document.getElementById('button')
+button.classList.add('tea');
+ 
+
+// button.style.padding='2px';
+// button.style.width='40%';
+// button.style.overscrollBehaviorInline='';
+// button.style.border='5px';
+// button.style.borderColor='white';
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"], // Adjust paths as needed
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
